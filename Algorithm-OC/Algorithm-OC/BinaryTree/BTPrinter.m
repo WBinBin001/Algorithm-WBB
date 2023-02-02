1909d7ceffedc2214504ad4db5b7df9b1e9adc1a
sky_tmp <sky_tmp@outlook.com>
2023-02-02 19:11:19 +0800

    二叉树 节点 x，y 值配置，添加打印测试

--- Algorithm-OC/Algorithm-OC/BinaryTree/BTPrinter.m
+++ Algorithm-OC/Algorithm-OC/BinaryTree/BTPrinter.m
@@ -22,16 +22,17 @@
 
 @implementation BTPrintNode
 
-+ (instancetype)initWithNode:(id)node
++ (instancetype)nodeWithNode:(id)node tree:(SearchBinaryTree *)tree
 {
-    return [[BTPrintNode alloc] initWithNode:node];
+    return [[BTPrintNode alloc] initWithNode:node tree:tree];
 }
 
-- (instancetype)initWithNode:(id)node
+- (instancetype)initWithNode:(id)node tree:(SearchBinaryTree *)tree
 {
     self = [super init];
     if(self){
         self.btNode = node;
+        self.width = [[tree string:node] description].length;
     }
     return self;
 }
@@ -45,6 +46,7 @@
 @property (nonatomic, strong) BTPrintNode *root;
 @property (nonatomic, strong) SearchBinaryTree *tree;
 @property (nonatomic, strong) NSMutableArray *nodes;
+@property (nonatomic, assign) NSUInteger maxWidth;
 
 @end
 
@@ -68,18 +70,27 @@
 
 - (void)configWithTree:(SearchBinaryTree *)tree
 {
-    self.root = [BTPrintNode initWithNode:[tree rootNode]];
+    self.root = [BTPrintNode nodeWithNode:[tree rootNode] tree:tree];
     self.tree = tree;
     self.nodes = [@[] mutableCopy];
+    self.maxWidth = [[self.tree string:[tree rootNode]] description].length;
 }
 
 - (void)print
 {
     [self addNodes];
-    [self _print];
+    [self configNodeOriginPoint];
+//    [self _print__001];
+    [self _print__002];
 }
 
-- (void)_print
+/*
+ 38
+ 18-69
+ 4-34-85
+ 29-36-71-100
+ */
+- (void)_print__001
 {
     NSLog(@"打印二叉树");
 
@@ -103,6 +114,42 @@
     NSLog(@"%@",string);
 }
 
+/*
+ ------------38-------------
+ ----18--------------69-----
+ -4---------34--------85----
+ 29------36------71------100
+ */
+- (void)_print__002
+{
+    NSString *normalString = @"";
+    NSArray *lastRows = self.nodes.lastObject;
+    NSUInteger space = self.maxWidth + 2;
+    NSUInteger lastRowLength = (lastRows.count - 1) * space + lastRows.count * self.maxWidth;
+        for (NSInteger i=0; i<lastRowLength; i++) {
+        normalString = [normalString stringByAppendingString:@"-"];
+    }
+    
+    NSString *newline = @"\n";
+    NSString *string = newline;
+
+    for (NSInteger i = 0; i < self.nodes.count; i++) {
+
+        NSArray *rows = self.nodes[i];
+        NSMutableString *currentLineString = [normalString mutableCopy];
+
+        for (NSInteger i=0; i<rows.count; i++) {
+            BTPrintNode *pNode = rows[i];
+            NSString *element = [[self.tree string:pNode.btNode] description];
+            [currentLineString replaceCharactersInRange:NSMakeRange(pNode.x, element.length) withString:element];
+        }
+        string = [string stringByAppendingString:currentLineString];
+        string = [string stringByAppendingString:newline];
+    }
+    NSLog(@"%@",string);
+}
+
+
 - (void)addNodes
 {
     
@@ -118,14 +165,16 @@
             id leftNode = [self.tree left:printNode.btNode];
             if(leftNode){
                 isStop = NO;
-                BTPrintNode *leftPNode = [BTPrintNode initWithNode:leftNode];
+                BTPrintNode *leftPNode = [BTPrintNode nodeWithNode:leftNode tree:self.tree];
+                self.maxWidth = MAX(leftPNode.width, self.maxWidth);
                 [curRows addObject:leftPNode];
             }
             
             id rightNode = [self.tree right:printNode.btNode];
             if(rightNode){
                 isStop = NO;
-                BTPrintNode *rightPNode = [BTPrintNode initWithNode:rightNode];
+                BTPrintNode *rightPNode = [BTPrintNode nodeWithNode:rightNode tree:self.tree];
+                self.maxWidth = MAX(rightPNode.width, self.maxWidth);
                 [curRows addObject:rightPNode];
             }
         }
@@ -134,6 +183,43 @@
         
         [self.nodes addObject:curRows];
         preRows = curRows;
+    }
+}
+
+
+- (void)configNodeOriginPoint
+{
+    NSArray *lastRows = self.nodes.lastObject;
+    NSUInteger space = self.maxWidth + 2;
+    NSUInteger lastRowLength = (lastRows.count - 1) * space + lastRows.count * self.maxWidth;
+    
+    for (NSInteger i=0; i<self.nodes.count; i++) {
+        NSArray *rows = self.nodes[i];
+        
+        NSUInteger tmpSpace = (lastRowLength - (rows.count - 1) * space) / rows.count;
+        NSUInteger nodeMargins = (tmpSpace - self.maxWidth) >> 1;
+        NSUInteger x = 0;
+
+        for (NSInteger j = 0; j<rows.count; j++) {
+            BTPrintNode *pNode = rows[j];
+
+            x = x + nodeMargins;
+
+            if(j){
+                x = x + space;
+            }
+            
+            pNode.x = x;
+            pNode.y = i;
+            x = x + self.maxWidth;
+            x = x + nodeMargins;
+            
+            NSLog(@"pNode = %@",[self.tree string:pNode.btNode]);
+            NSLog(@"pNode.x = %ld",pNode.x);
+            NSLog(@"pNode.y = %ld",pNode.y);
+            NSLog(@"");
+        }
+        
     }
 }
 
